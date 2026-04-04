96c3e01b88e72d1e0b0ec9cac71be859381c33a8
decaffeinated-tryhard <hitarthkaul@gmail.com>
2026-04-04 13:52:36 +0530

    timer works

--- timer.py
+++ timer.py
@@ -1,6 +1,35 @@
 #holy shit i pushed to git anyways let's code now
 
 import tkinter as tk 
+import time
 
-root = tk.Tk()
-root.mainloop()
+#timer function
+#i just discovered an edge case while trying to find ways to measure time elasped
+#if the system time changes while the timer is running it can cause whacky behaviour
+#nothing to worry about rn but definitely in future
+#anyways
+
+start_sec = 0
+new_sec = 0
+seconds = 0
+
+
+
+    
+start_sec = int(time.time())
+for i in range (100000000):
+
+    new_sec= int(time.time())
+    seconds = new_sec - start_sec 
+    print (seconds)
+    
+#timer works BARELY
+
+
+
+# #tk boilerplate
+# root = tk.Tk()
+# root.title("timer")
+# button = tk.Button(root, text="start", width=25, command=root.destroy)
+# button.pack()
+# root.mainloop()
