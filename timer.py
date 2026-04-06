055bd3e045c701429c93215e32ad740372eab5d4
decaffeinated-tryhard <hitarthkaul@gmail.com>
2026-04-06 22:35:33 +0530

    functions work

--- timer.py
+++ timer.py
@@ -20,24 +20,45 @@ import time
 #     print (seconds)
     
 #timer works BARELY
+seconds=0
+cur_time = 0
+
 def update():
     if is_running == True:
-        seconds = start_timer - int(time.time())
-
+        global seconds
+        global cur_time
+        cur_time=int(time.time())
+        seconds = start_time - cur_time
+        print (is_running)
+        print(start_time)
+        print(cur_time)
 
 is_running=False
+
+start_time=0
+
 def start_timer():
+    global is_running
     is_running=True
+    global start_time
     start_time= int(time.time())
     update()
 
+def printfffff():
+    print (is_running)
+    print(start_time)
+    print(cur_time)
 
 
 # #tk boilerplate
 root = tk.Tk()
-label-tk.Label(root,text=seconds)
 
+label=tk.Label(root,text=seconds)
+label.pack()
 root.title("timer")
-button = tk.Button(root, text="start", width=25, command=start_timer())
+button = tk.Button(root, text="start", width=25, command=start_timer)
 button.pack()
+button = tk.Button(root, text="debug", width=25, command=printfffff)
+button.pack()
+
 root.mainloop()
